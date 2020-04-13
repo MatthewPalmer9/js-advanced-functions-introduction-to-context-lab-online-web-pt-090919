13a07d5be2819a6a9134f9773a956f5a18d88ff7
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2020-04-13 20:51:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -49,5 +49,5 @@ function wagesEarnedOnDate(empRecord, date){
 
 function allWagesFor(empRecord){
   const datesWorked = empRecord.timeInEvents.map(event => event.date)
-  const wagesOnDatesWorked = empRecord.
+  const wagesOnDatesWorked = datesWorked.map()
 }
